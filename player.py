b6061bfbda038e1e67f8e9c63a2fba4c01a5fee0
Rupika <rupika.d97@gmail.com>
2020-02-29 17:09:15 -0800

    added functions to player

--- player.py
+++ player.py
@@ -7,13 +7,30 @@ March 13, 2020
 ***********************************
 """
 
-class player():
-    def __init__(self):
+
+class Player:
+    def __init__(self, deck):
         self.hand = []
-        self.memory = [[]]
+        self.memory = deck
         self.balance = 0
+        self.start_bet = 0
+        self.current_bet = 0
+        self.played = False
 
     def hit(self):
 
         return
-	
+
+    def stand(self):
+        return
+
+    def split(self):
+        return
+
+    def double_down(self):
+        return
+
+    def surrender(self):
+        return
+
+
